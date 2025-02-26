2422ea2ca4aa53f9f344b36f2815882633c46b48
Skai Nzeuton <san82@cornell.edu>
2025-02-26 01:05:29 -0500

    Clean up main(), use correct base for radix sort, make counting sort more efficient

--- sort/sort.java
+++ sort/sort.java
@@ -2,49 +2,37 @@ package sort;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class sort{
-    public static int[] countingSort(int[] nums, int place) {
+public class sort {
+    public static ArrayList<Integer> countingSort(ArrayList<Integer> nums, int place) {
         // Counting sort
-        int max = digitAtPlace(nums[0], place);
-        for (int i = 0; i < nums.length; i++) {
-            if (digitAtPlace(nums[i], place) > max) {
-                max = digitAtPlace(nums[i], place);
-            }
+        int[] countArray = new int[10];
+
+        for (int i = 0; i < nums.size(); i++) {
+            countArray[digitAtPlace(nums.get(i), place)]++;
         }
 
-        int[] countArray = new int[max + 1];
-
-        for (int i = 0; i < countArray.length; i++) {
-            int freq = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (digitAtPlace(nums[j], place) == i) {
-                    freq++;
-                }
-            }
-
-            if (i == 0) {
-                countArray[i] = freq;
-            } else {
-                countArray[i] = countArray[i - 1] + freq;
-            }
+        for (int i = 1; i < countArray.length; i++) {
+            countArray[i] = countArray[i] + countArray[i - 1];
+        }
+            
+        Integer[] sorted = new Integer[nums.size()];
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int digit = digitAtPlace(nums.get(i), place);
+            countArray[digit]--;
+            sorted[countArray[digit]] = nums.get(i);
         }
 
-        int[] sorted = new int[nums.length];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            sorted[countArray[digitAtPlace(nums[i], place)] - 1] = nums[i];
-            countArray[digitAtPlace(nums[i], place)]--;
-        }
-
-        return sorted;
+        return new ArrayList<>(Arrays.asList(sorted));
     }
 
     public static int digitAtPlace(int num, int place) {
         return (int) (num / Math.pow(10, place) % 10);
     }
 
-    public static int[] radixSort(int[] nums) {
-        int length = (int) (Math.log(nums[0]));
+    public static ArrayList<Integer> radixSort(ArrayList<Integer> nums) {
+        int length = (int) Math.ceil(Math.log10(Collections.max(nums))) + 1;
         for (int i = 0; i < length; i++) {
             nums = countingSort(nums, i);
         }
@@ -53,70 +41,30 @@ public class sort{
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> input = new ArrayList<Integer>();
-        int numPos = 0;
-        int numNeg = 0;
+        ArrayList<Integer> posNums = new ArrayList<Integer>(600000);
+        ArrayList<Integer> negNums = new ArrayList<Integer>(600000);
+
         while (in.hasNextLine()) {
             String nextline = in.nextLine().replaceAll("[^0-9-]", "");
-            input.add(Integer.parseInt(nextline));
             int value = Integer.parseInt(nextline);
-            if (value< 0) {
-                numNeg++;
+
+            if (value < 0) {
+                negNums.add(-value);
             }
             else {
-                numPos++;
+                posNums.add(value);
             }
         }
         
+        ArrayList<Integer> posSorted = radixSort(posNums);
+        ArrayList<Integer> negSorted = radixSort(negNums);
 
-        int[] posInput = new int[numPos];
-        int[] negInput = new int[numNeg];
-        int posCounter = 0;
-        int negCounter = 0;
-        for (int i = 0; i < input.size(); i++) {
-            if (input.get(i) < 0) {
-                negInput[negCounter] = -input.get(i);
-                negCounter++;
-            }
-            else {
-                posInput[posCounter] = input.get(i);
-                posCounter++;
-            }
+        for (int i = negSorted.size() - 1; i >= 0; i--) {
+            System.out.println(-negSorted.get(i));
         }
-        int[] posSorted;
-        int[] negSorted;
-        if (posCounter != 0) {
-            posSorted = radixSort(posInput);
-        }
-        else {
-            posSorted = new int[0];
-        }
-
-        if (negCounter != 0) {
-            negSorted = radixSort(negInput);
-        }
-        else {
-            negSorted = new int[0];
-        }
-
-        int[] output = new int[posSorted.length+negSorted.length];
-
-        negCounter = negSorted.length - 1;
-        int counter = 0;
-        while (counter < input.size()) {
-            if (counter < negSorted.length) {
-                output[counter] = -negSorted[negCounter];
-                negCounter--;
-            }
-            else {
-                output[counter] = posSorted[counter - negSorted.length];
-            }
-            counter += 1;
-        }
-
         
-        for (int i = 0; i < output.length; i++) {
-            System.out.println(output[i]);
+        for (int i = 0; i < posSorted.size(); i++) {
+            System.out.println(posSorted.get(i));
         }
     }
 }
